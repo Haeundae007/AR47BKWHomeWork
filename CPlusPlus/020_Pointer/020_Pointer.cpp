eff141680569f8d1654acbf25c9a1a1c00cb961f
Kiwon <2611754@naver.com>
2023-03-21 11:31:36 +0900

    ㅇㅇㅇ

---
+++ CPlusPlus/020_Pointer/020_Pointer.cpp
@@ -0,0 +1,24 @@
+﻿// 020_Pointer.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	
+
+	int Value = 0;
+
+	int* ValuePtr = &Value;
+
+	__int64 Address = 0;
+
+	Address = (__int64)ValuePtr;
+
+	int a = 0;
+
+}
+
+//포인터란 c++의 자료형의 한 형태이며 자료형 뒤에 *을 붙이는 문법을 통해서 선언한다
+
+//value가 표현하는 값이 2가지. 내부의 자신의 진짜값10, 램에 존재하게 되는 자신의 위치인 100번지
---
+++ CPlusPlus/021_Pointer001/021_Pointer001.cpp
@@ -0,0 +1,28 @@
+﻿// 021_Pointer001.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+    {
+        int Arr[3] = {};
+
+        // Arr n번지라면
+        // n번지 + sizeof(int) * 정수
+        int* ValuePtr0 = &Arr[0];
+        int* ValuePtr1 = &Arr[1];
+        int* ValuePtr2 = &Arr[2];
+
+        __int64 Address0 = 0;
+        __int64 Address1 = 0;
+        __int64 Address2 = 0;
+
+        Address0 = (__int64)ValuePtr0;
+        Address1 = (__int64)ValuePtr1;
+        Address2 = (__int64)ValuePtr2;
+        int a = 0;
+
+    }
+}
+
